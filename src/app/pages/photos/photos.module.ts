eb20c86776ed857da3d3eb84ed749dc26adcca9c
VladimirMinenko <minvladserg@gmail.com>
2022-10-09 18:05:50 +0300

    Add NzImageModule to page photos

--- src/app/pages/photos/photos.module.ts
+++ src/app/pages/photos/photos.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { PhotosRoutingModule } from './photos-routing.module';
 import { PhotosComponent } from './photos.component';
 import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzImageModule } from 'ng-zorro-antd/image';
 
 @NgModule({
   declarations: [PhotosComponent],
-  imports: [CommonModule, PhotosRoutingModule, NzTableModule],
+  imports: [CommonModule, PhotosRoutingModule, NzTableModule, NzImageModule],
   exports: [PhotosComponent],
 })
 export class PhotosModule {}
