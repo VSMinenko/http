c2b0e549626b9e969ddc63da68e27938b457dfbd
VladimirMinenko <minvladserg@gmail.com>
2022-10-10 20:17:39 +0300

    Add Modal dialogs and Popover

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NzTableModule } from 'ng-zorro-antd/table';
 import { NzImageModule } from 'ng-zorro-antd/image';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzPopoverModule } from 'ng-zorro-antd/popover';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [CommonModule, NzTableModule, NzImageModule],
+  exports: [
+    CommonModule,
+    NzTableModule,
+    NzImageModule,
+    NzModalModule,
+    NzPopoverModule,
+    NzButtonModule,
+  ],
 })
 export class SharedModule {}
